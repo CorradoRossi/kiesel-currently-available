22ebb112c6cc011e441007b98dd93809cccb3827
CorradoRossi <rossimeacham@gmail.com>
2020-03-28 20:55:57 -0400

    add support for basses

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import Guitars from './views/Guitarsv2'
+import Guitars from './views/Guitars'
+import Basses from './views/Basses'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { HashRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Guitars />
-      <Footer />
-    </div>
+    <HashRouter basename="/">
+      <div className="App">
+        <Header />
+        <Route exact path="/" component={Guitars} />
+        <Route path="/bass-guitars" component={Basses} />
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import logo from '../assets/images/logo-cg.png'
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="d-inline-flex header pt-5 pb-4 w-100">
+    <div className="d-flex justify-content-around header pt-5 pb-4 px-lg-2 mx-lg-5">
       <a href="https://www.kieselguitars.com/" className="pl-5 mr-auto">
         <img src={logo} height="40" />
       </a>
-      <p className="pr-5 ml-auto"><span className="font-weight-bold">(Unofficial)</span> Currently Available Kiesel Guitars List</p>
-      <div className={"d-flex pb-4"}>
-        <div className={"d-none d-lg-block get-started"}>
-          <a href={"https://getmarlo.com"}>Basses</a>
+      <p className="mx-auto pt-2"><span className="font-weight-bold">(Unofficial)</span></p>
+      <div className="d-flex">
+        <div className={"d-flex pb-4"}>
+          <div className={"d-block get-started"}>
+            <Link to="/">Guitars</Link>
+          </div>
+        </div>
+        <div className={"d-flex pb-4 px-4"}>
+          <div className={"d-block get-started"}>
+            <Link to="/bass-guitars">Basses</Link>
+          </div>
         </div>
       </div>
     </div>
